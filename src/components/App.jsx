60ddf9c3beb0d509bc0cb16203b1650dee5c77e3
tarek-khalifa-1 <user.tarek.dev@gmail.com>
2026-01-27 14:50:36 +0200

    init code

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import Header from "./Header";
+
+export default function App() {
+  return (
+    <div className="app">
+      <Header />
+      <main className="main"></main>
+    </div>
+  );
+}
